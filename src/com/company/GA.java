48c6b2c2e4e177d4eb3eda6daf08dcc5491aff3a
spieriz <spieriz@gmail.com>
2019-03-14 22:15:25 +0100

    Removed methods from GA class which should be implemented in TTP class. Added enum KNP_TYPE. Added calculation of current velocity. Added method to calculate KNP. Added method to calculate travel time that includes variable travel velocity.

--- src/com/company/GA.java
+++ src/com/company/GA.java
@@ -1,69 +1,8 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 /*
 Class GA is used to implement all needed genetic algorithms and solve a TTP problem
  */
 class GA {
 
-    // reference to created TTP object, which is storing all data from parsed file
-    private TTP ttp;
-
-    /*
-    TSP is an array list used to store indexes of the nodes in specific order.
-    Tt should be created randomly at the very beginning
-    */
-    private ArrayList<Integer> tsp;
-
-    GA(TTP ttp){
-        this.ttp = ttp;
-
-        tsp = new ArrayList<>();
-    }
-
-    private double getDistanceBetweenNodes(int indexA, int indexB){
-        /*
-        Decrementing of index is needed due to way of indexing array and nodes:
-        Arrays are indexed from 0 and nodes are indexed from 1
-         */
-        return ttp.getDistanceValue(indexA - 1, indexB - 1);
-    }
-
-    double calculateTravelTime(double velocity){
-        double travelTime = 0.0;
-
-        for (int i = 1 ; i < tsp.size(); i++){
-            travelTime += getDistanceBetweenNodes(tsp.get(i - 1), tsp.get(i)) / velocity;
-        }
-
-        return travelTime;
-    }
-
-    // fill tsp array with randomly placed indexes of nodes
-    void createRandomTSP(){
-        tsp.clear();
-
-        // fill an ArrayList with indexes of available nodes (1..N)
-        for (int i = 1 ; i <= ttp.getDimensionsNumber(); i++){
-            tsp.add(i);
-        }
-
-        // swap indexes randomly
-        Collections.shuffle(tsp);
-    }
-
-
-    // toString methods zone
-
-    String tspToString(){
-        StringBuilder result = new StringBuilder();
-
-        for (int index : tsp){
-            result.append(index).append(" ");
-        }
-
-        return result.toString();
-    }
 }
--- src/com/company/TTP.java
+++ src/com/company/TTP.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /*
 Class TTP is used to store all data from parsed file
@@ -20,26 +21,181 @@ public class TTP {
     private ArrayList<NodeData> nodesList;
     private ArrayList<ItemData> itemsList;
 
-    private double distanceMatrix[][];
+    private double[][] distanceMatrix;
 
+    private ArrayList<Integer> tsp;
+    private ArrayList<Double> velocityList;
 
+    private enum KNP_TYPE {
+        HIGHEST_VALUE,  // Always take an element with the highest value
+        LOWEST_WEIGHT,  // Always take an element with the lowest weight
+        BEST_RATIO,     // Always take an element with the highest value/weight ratio
+    }
+
+    private KNP_TYPE knpType;
+
+    /*
+    Just a constructor :)
+     */
     TTP(){
         nodesList = new ArrayList<>();
         itemsList = new ArrayList<>();
-    }
 
-
-    double getDistanceValue(int indexA, int indexB){
-        return distanceMatrix[indexA][indexB];
+        tsp = new ArrayList<>();
+        velocityList = new ArrayList<>();
     }
 
     /*
-    Calculates Euclidean distance between dwo points in 2D space
+    --------------------------------------------------------------------------------------------------
+    */
+
+    private ItemData getBestItem(ArrayList<ItemData> itemsInNode) {
+        int currentBestIndex = 0;
+
+        switch (knpType){
+            case HIGHEST_VALUE:
+
+                for (int i = 0; i < itemsInNode.size(); i++) {
+                    if (itemsInNode.get(i).getProfit() > itemsInNode.get(currentBestIndex).getProfit())
+                        currentBestIndex = i;
+                }
+
+                return itemsInNode.get(currentBestIndex);
+
+            case LOWEST_WEIGHT:
+
+                for (int i = 0; i < itemsInNode.size(); i++) {
+                    if (itemsInNode.get(i).getWeight() < itemsInNode.get(currentBestIndex).getWeight())
+                        currentBestIndex = i;
+                }
+
+                return itemsInNode.get(currentBestIndex);
+
+            case BEST_RATIO:
+
+                for (int i = 0; i < itemsInNode.size(); i++) {
+                    if (itemsInNode.get(i).getProfit() / itemsInNode.get(i).getWeight() >
+                            itemsInNode.get(currentBestIndex).getProfit() / itemsInNode.get(currentBestIndex).getWeight())
+
+                        currentBestIndex = i;
+                }
+
+                return itemsInNode.get(currentBestIndex);
+
+        }
+        return new ItemData(0, 0, 0, 0);
+    }
+
+    private double calculateCurrentVelocity(int currentWeight){
+        return getMaxSpeed() - currentWeight * ((getMaxSpeed() - getMinSpeed()) / getCapacity());
+    }
+
+    int calculateKNP(){
+        int totalValue = 0;
+        int currentWeight = 0;
+
+        // for each node in tsp arrayList calculate best option
+        for (int currentNodeID : tsp){
+
+            // arrayList for all items in node
+            ArrayList<ItemData> itemsInNode = new ArrayList<>();
+
+            /*
+            From list of all items, choose these which are available in current node (currentNodeID)
+             */
+            for (ItemData currentItem : itemsList) {
+                if (currentItem.getNode() == currentNodeID){
+                    itemsInNode.add(currentItem);
+                }
+            }
+
+            /*
+            If items exists in this node, take the best and update knapsack value and weight
+             */
+            if (itemsInNode.size() > 0){
+                ItemData bestItem = getBestItem(itemsInNode);
+
+                totalValue += bestItem.getProfit();
+                currentWeight += bestItem.getWeight();
+            }
+
+            velocityList.add(calculateCurrentVelocity(currentWeight));
+        }
+
+        return totalValue;
+    }
+
+    double calculateTrip(){
+        double travelTime = 0.0;
+
+        // for each node in tsp arrayList calculate everything
+        for (int i = 1 ; i < tsp.size(); i++){
+            travelTime += getDistanceBetweenNodes(tsp.get(i - 1), tsp.get(i)) / velocityList.get(i - 1);
+        }
+
+        // travel time between last and first node
+        travelTime += getDistanceBetweenNodes(tsp.get(0), tsp.get(tsp.size() - 1)) / velocityList.get(velocityList.size() - 1);
+
+        return travelTime;
+    }
+
+    /*
+    Calculates travel time from first to last node (and last->first)
+     */
+    double calculateTravelTime(double velocity){
+        double travelTime = 0.0;
+
+        for (int i = 1 ; i < tsp.size(); i++){
+            travelTime += getDistanceBetweenNodes(tsp.get(i - 1), tsp.get(i)) / velocity;
+        }
+
+        // travel time between last and first node
+        travelTime += getDistanceBetweenNodes(tsp.get(0), tsp.get(tsp.size() - 1));
+
+        return travelTime;
+    }
+
+    /*
+    Get distance between two nodes
+     */
+    private double getDistanceBetweenNodes(int indexA, int indexB){
+        /*
+        Decrementing of index is needed due to way of indexing array and nodes:
+        Arrays are indexed from 0 and nodes are indexed from 1
+        */
+        return distanceMatrix[indexA - 1][indexB - 1];
+    }
+
+    /*
+    --------------------------------------------------------------------------------------------------
+    */
+
+    /*
+    Fill tsp array with randomly placed indexes of nodes
+     */
+    void createRandomTSP(){
+        tsp.clear();
+
+        // fill an ArrayList with indexes of available nodes (1..N)
+        for (int i = 1 ; i <= getDimensionsNumber(); i++){
+            tsp.add(i);
+        }
+
+        // swap indexes randomly
+        Collections.shuffle(tsp);
+    }
+
+    /*
+    Calculates Euclidean distance between dwo points in 2D space.
+    Used only to create distance matrix.
      */
     private double getDistanceBetweenPoints(double x1, double y1, double x2, double y2){
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
+    /*
+    Calculating distance matrix for all nodes (using getDistanceBetweenPoints())
+     */
     void setDistanceMatrix() {
         distanceMatrix = new double[dimensionsNumber][dimensionsNumber];
 
@@ -59,7 +215,9 @@ public class TTP {
         }
     }
 
-    // toString methods zone
+    /*
+    --------------------------------------------------------------------------------------------------
+     */
 
     String headersToString(){
         return  "Problem name: " + problemName + "\n" +
@@ -92,8 +250,29 @@ public class TTP {
         return result;
     }
 
+    String tspToString(){
+        StringBuilder result = new StringBuilder();
 
-    // getters and setters zone
+        for (int index : tsp){
+            result.append(index).append(" ");
+        }
+
+        return result.toString();
+    }
+
+    String velocityToString(){
+        StringBuilder result = new StringBuilder();
+
+        for (double velocity : velocityList){
+            result.append(velocity).append(" ");
+        }
+
+        return result.toString();
+    }
+
+    /*
+    --------------------------------------------------------------------------------------------------
+    */
 
     public String getProblemName() {
         return problemName;
@@ -175,4 +354,15 @@ public class TTP {
         this.itemsList = itemsList;
     }
 
+    void setKnpTypeHighestValue(){
+        knpType = KNP_TYPE.HIGHEST_VALUE;
+    }
+
+    void setKnpTypeLowestWeight(){
+        knpType = KNP_TYPE.LOWEST_WEIGHT;
+    }
+
+    void setKnpTypeBestRatio(){
+        knpType = KNP_TYPE.BEST_RATIO;
+    }
 }
