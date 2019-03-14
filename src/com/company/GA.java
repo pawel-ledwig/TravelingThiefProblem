6cb964cc96654b331666bce5128b9ef6d794a1a3
spieriz <spieriz@gmail.com>
2019-03-15 00:14:29 +0100

    Added methods to get worst and best specimens rating

--- src/com/company/GA.java
+++ src/com/company/GA.java
@@ -7,6 +7,7 @@ Class GA is used to implement all needed genetic algorithms and solve a TTP prob
  */
 class GA {
     private int populationSize;
+    private int generationsNumber;
     private ArrayList<Specimen> specimensList;
 
     private TTP ttp;
@@ -18,6 +19,33 @@ class GA {
         specimensList = new ArrayList<>();
     }
 
+    double getBestRating(){
+        double bestRating = specimensList.get(0).getRating();
+
+        for (Specimen specimen : specimensList){
+            if (specimen.getRating() > bestRating){
+                bestRating = specimen.getRating();
+            }
+        }
+
+        return bestRating;
+    }
+
+    double getWorstRating(){
+        double worstRating = specimensList.get(0).getRating();
+
+        for (Specimen specimen : specimensList){
+            if (specimen.getRating() < worstRating){
+                worstRating = specimen.getRating();
+            }
+        }
+
+        return worstRating;
+    }
+
+    /*
+    Create random population
+     */
     void createPopulation(){
         for (int i = 0; i < populationSize; i++){
             specimensList.add(ttp.createRandomSpecimen());
@@ -32,5 +60,17 @@ class GA {
         }
 
         return result.toString();
+    }
+
+    /*
+    --------------------------------------------------------------------------------------------------
+    */
+
+    public int getGenerationsNumber() {
+        return generationsNumber;
+    }
+
+    public void setGenerationsNumber(int generationsNumber) {
+        this.generationsNumber = generationsNumber;
     }
 }
